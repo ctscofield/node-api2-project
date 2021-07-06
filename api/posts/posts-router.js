f4ce680dbc401bdefbdead643db46110e3400527
ctscofield <ctscofield@gmail.com>
2021-07-06 14:58:16 -0600

    get by id endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,6 +19,25 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/:id", (req, res) => {
+    Posts.findById(req.params.id)
+        .then(posts => {
+            if (!posts) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json(posts);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved",
+                error: err.message
+            });
+        });
+});
+
+
 
 
 module.exports = router;
